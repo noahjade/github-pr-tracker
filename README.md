27cd1e398acfd518d1bae613305bb382fc82863e
Noah <hwil965@aucklanduni.ac.nz>
2021-10-05 15:38:26 +1300

    Getting inputs
    - Added inquirer as a dependency
    - User is now prompted for the repo name and owner

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Packages used:
 Chalk - for colouring and formatting the command line output.
 Figlet - for creating ASCII art from text.
 Clear - to clear the terminal screen and keep it clean.
+Inquirer - for command line i/o
 @octokit/rest - a Github REST API client for Node.js.
--- src/index.js
+++ src/index.js
@@ -2,12 +2,50 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const octokit = require('@octokit/rest');
+const inquirer = require('inquirer');
 
-clear();
 
-console.log(
-  // Hex colour taken from Multitudes website
-  chalk.hex('#6DD8AA')(
-    figlet.textSync('Multitudes CLI', { horizontalLayout: 'fitted' })
-  )
-);
+/** Helper function to print welcome message */ 
+function printWelcome(){
+    console.log(
+        chalk.hex('#24B88F')(
+          figlet.textSync('Welcome to', { horizontalLayout: 'fitted' }) //todo: format these nicely
+        )
+    );
+
+    console.log(
+        // Hex colour taken from Multitudes website
+        chalk.hex('#6DD8AA')(
+          figlet.textSync('Multitudes CLI', { horizontalLayout: 'fitted' })
+        )
+    );
+}
+
+
+function Main(){
+    clear();
+
+    printWelcome();
+
+    var questions = [
+        {
+          type: 'input',
+          name: 'owner',
+          message: "Who is the repo owner?"
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the repo name?"
+        }
+      ]
+      
+    inquirer.prompt(questions).then(answers => {
+        console.log(`Counting open PR requests for ${answers['owner']}/${answers['name']}!`)
+    
+    })
+
+}
+
+
+Main();
