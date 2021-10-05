35b2b55266881809a3fa24e7d4bb7bcdcdc83e75
Noah <hwil965@aucklanduni.ac.nz>
2021-10-05 16:24:48 +1300

    Basic API query

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const octokit = require('@octokit/rest');
 const inquirer = require('inquirer');
+
+
+const { Octokit } = require('@octokit/rest');
+const octokit = new Octokit({
+    baseUrl: 'https://api.github.com'
+});
 
 
 /** Helper function to print welcome message */ 
@@ -19,6 +24,27 @@ function printWelcome(){
           figlet.textSync('Multitudes CLI', { horizontalLayout: 'fitted' })
         )
     );
+}
+
+async function countPRs(repoOwner, repoName) {
+
+    // The GitHub API limits results to 100 a request, so we need to iterate through the pages to count all PRs.
+    // The maximum amount (100) are fetched per page to minimise requests and avoid throttling.
+
+    // todo: handle 404 errors
+
+    /* Test data to play with:
+        octocat hello-world
+        microsoft typescript
+    */
+
+    await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+        owner: repoOwner,
+        repo: repoName
+    }).then(({ data }) => {
+        console.log(`we got: ${data.length}`);
+    });
+
 }
 
 
@@ -42,7 +68,9 @@ function Main(){
       
     inquirer.prompt(questions).then(answers => {
         console.log(`Counting open PR requests for ${answers['owner']}/${answers['name']}!`)
-    
+        
+        countPRs(answers['owner'], answers['name']);
+
     })
 
 }
